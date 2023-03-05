5031d3a2a8f42e63cd40852cc23e7b838dcd5f34
Christian M. Tania <taniachristian01@gmail.com>
2023-03-05 21:41:37 +0800

    Auto-generated commit [ci skip]

--- src/features/shared/model/experience-model.ts
+++
@@ -1,8 +0,0 @@
-export class WorkExperience {
-    public ExperienceID: number = 0;
-    public Company: string = '';
-    public Year: string = '';
-    public Title: string = '';
-    public JobDescription: Array<string> = [];
-}
-
--- src/features/shared/model/skill-model.ts
+++
@@ -1,8 +0,0 @@
-export class SkillModel {
-    public ProgrammingLanguages: string = '';
-    public Frameworks: string = '';
-    public CloudPlatforms: string = '';
-    public Tools: string = '';
-    public Database: string = '';
-}
-
